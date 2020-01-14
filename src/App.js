2a86fc320833c4948838dbfe92052f38f945e1c4
Alex Munoz <luismunoz@Luiss-MacBook-Air.local>
2020-01-13 20:53:41 -0600

    render page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Contacts from './Components/Contacts'
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component  {
+  state = {
+  users : []
+  };
+
+  getUsers = () => {
+    fetch("https://randomuser.me/api?results=25")
+    .then(response => response.json())
+    .then(data => this.setState({users : data}));
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  render() {
+    console.log(this.state.users.results)
+    return (
+      <div>
+        hello
+      </div>
+    )
+    // return (
+    //   <div className="App">
+    //     {this.state.users.data.results.map((user, index) => (
+    //     <Contacts        
+    //     />
+    //     ))}
+    //   </div>
+    // );
+  }
 }
+
 
 export default App;
 
